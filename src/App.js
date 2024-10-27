a7ff29730cd09c1c866d88fad71e7bad33fa36f3
yiyujin <yujin94@gmail.com>
2024-10-27 07:11:30 -0400

    add method - print history

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ class App {
   async run() {
     try {
       let input = await Console.readLineAsync("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)\n");
+      const input2 = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
 
       input = input.split(",");
 
       this.handleErrors(input);
+      const data = this.printHistory(input, input2);
 
     } catch (error) {
       if (!error.message.startsWith("[ERROR]")) {
@@ -34,6 +36,38 @@ class App {
       }
     }
   }  
+
+  printHistory(input, input2){
+    let data = [];
+
+    for(let i = 0; i < input.length; i++){
+      data.push({
+        name : input[i],
+        scores : "",
+      })
+    }
+
+      Console.print("\n실행 결과");
+
+      for(let i = 0; i < input2; i++){
+
+        for(let j = 0; j < data.length; j++){
+          let randomNumber = Random.pickNumberInRange(0,9);
+
+          if(randomNumber >= 4){
+            data[j].scores += "-";
+          } else {
+            data[j].scores += "";
+          }
+
+          Console.print(`${data[j].name} : ${data[j].scores}`)
+        }
+
+        Console.print('\n');
+      }
+
+      return data;
+  }
 
 }
 
